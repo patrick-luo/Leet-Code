36e3b43adb5b207b33399876b0b224a85067df70
patrick-luo <patrick.luo2009@gmail.com>
2018-04-30 17:46:09 -0400

    Create 653. Two Sum IV - Input is a BST.py

---
+++ 653. Two Sum IV - Input is a BST.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+"""The key idea is to use a memo to record visited
+values and use a sub-function find() to locate the
+other element if applicable.
+
+Note: this solution works for any binary trees
+other than a BST."""
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        def find(root, k, memo):
+            if root is None:
+                return False
+            if (k-root.val) in memo:
+                return True
+            memo.add(root.val)
+            return find(root.left, k, memo) or find(root.right, k, memo)
+        
+        memo = set()
+        return find(root, k, memo)
