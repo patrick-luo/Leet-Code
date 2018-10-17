791f1b0c25734ec1a00d9a6b810f26f755e6f468
patrick-luo <patrick.luo2009@gmail.com>
2018-10-17 19:28:33 -0400

    Facebook: just do dfs

---
+++ 257. Binary Tree Paths.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        def dfs(res, nodes, root):
+            if root is None:
+                return
+            nodes.append(root)
+            if root.left is None and root.right is None:
+                res.append(getPath(nodes))
+            else:
+                dfs(res, nodes, root.left)
+                dfs(res, nodes, root.right)
+            nodes.pop()
+        
+        def getPath(nodes):
+            path = str(nodes[0].val)
+            for n in nodes[1:]:
+                path += '->' + str(n.val)
+            return path
+                
+        
+        res = list()
+        nodes = list()
+        dfs(res, nodes, root)
+        return res
