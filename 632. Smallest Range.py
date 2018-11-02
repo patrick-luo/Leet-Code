029f97c54a4cf913d42036a4d22526181b804e4d
patrick-luo <patrick.luo2009@gmail.com>
2018-11-02 10:22:39 -0400

    Use a heap to keep track of the current elements from all the lists

--- 632. Smallest Range.py
+++ 632. Smallest Range.py
@@ -1,31 +1,29 @@
-"""Not sure why time limit exceeded
-"""
-
 class Solution(object):
     def smallestRange(self, nums):
         """
         :type nums: List[List[int]]
         :rtype: List[int]
         """
-        def smaller(this, that):
-            return (this[1]-this[0]) < (that[1]-that[0]) or \
-                (this[1]-this[0]) == (that[1]-that[0]) and this[0] < that[0]
+        def smaller(p, q):
+            return (p[1]-p[0])<(q[1]-q[0]) or \
+                (p[1]-p[0])==(q[1]-q[0]) and p[0]<q[0]
         
+        minR = [sys.float_info.max, -sys.float_info.max]
         pq = list()
-        curR = [sys.float_info.max, -sys.float_info.max]
         for array in nums:
-            curR[0] = min(curR[0], array[0])
-            curR[1] = max(curR[1], array[0])
             heapq.heappush(pq, (array[0], array, 0))
-        minR = [curR[0], curR[1]]
+            minR[0] = min(minR[0], array[0])
+            minR[1] = max(minR[1], array[0])
+        curR = [minR[0], minR[1]]
         
         while True:
             val, array, i = heapq.heappop(pq)
-            if i < len(array)-1:
-                heapq.heappush(pq, (array[i+1], array, i+1))
-                curR[0] = heapq.nsmallest(1, pq)[0][0]
-                curR[1] = max(curR[1], array[i+1])
-                if smaller(curR, minR):
-                    minR = [curR[0], curR[1]]
-            else:
+            if i == len(array)-1:
                 return minR
+            else:
+                heapq.heappush(pq, (array[i+1], array, i+1))
+                curR[1] = max(curR[1], array[i+1])
+                curR[0] = pq[0][0]
+                if smaller(curR, minR):
+                    minR[0] = curR[0]
+                    minR[1] = curR[1]
