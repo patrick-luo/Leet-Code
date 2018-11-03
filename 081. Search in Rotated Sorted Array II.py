76a9270d954bee1e199a82f62cc1b4c268716759
patrick-luo <patrick.luo2009@gmail.com>
2018-11-03 09:44:58 -0400

    Hard! Worth going into it. Check nums[left] ? nums[mid] to determine if some half is ordered

--- 081. Search in Rotated Sorted Array II.py
+++ 081. Search in Rotated Sorted Array II.py
@@ -1,7 +1,3 @@
-"""The current implementation is not working properly,
-but can show the idea.
-"""
-
 class Solution(object):
     def search(self, nums, target):
         """
@@ -10,46 +6,34 @@ class Solution(object):
         :rtype: bool
         """
         def bsearch(nums, target, low, high):
-            while low <= high:
-                mid = low + (high-low)/2
-                if nums[mid] == target:
-                    return True
-                elif nums[mid] < target:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            return False
-        
-        def findPivot(nums):
-            low, high = 0, len(nums)-1
-            while low <= high:
-                if nums[low] < nums[high]:
-                    return None
-                mid = low + (high-low)/2
-                if mid == len(nums) - 1:
-                    return None
-                if nums[mid] > nums[mid+1]:
-                    return mid
-                if nums[mid] < nums[high]:
-                    high = mid - 1
-                elif nums[mid] > nums[low]:
-                    low = mid + 1
-                else:
-                    if low == len(nums) - 1:
-                        return None
-                    if nums[low] > nums[low+1]:
-                        return low
-                    else:
-                        low += 1
-        
-        
-        pivot = findPivot(nums)
-        if pivot is None:
-            return bsearch(nums, target, 0, len(nums)-1)
-        else:
-            if nums[0] <= target <= nums[pivot]:
-                return bsearch(nums, target, 0, pivot)
-            elif nums[pivot+1] <= target <= nums[-1]:
-                return bsearch(nums, target, pivot+1, len(nums)-1)
-            else:
+            if high < low:
                 return False
+            mid = (low+high) / 2
+            if nums[mid] == target:
+                return True
+            if nums[low] < nums[mid]: # the left half is ordered
+                if nums[low] <= target < nums[mid]:
+                    return bsearch(nums, target, low, mid-1)
+                else:
+                    return bsearch(nums, target, mid+1, high)
+            elif nums[low] > nums[mid]: # the right half is ordered
+                if nums[mid] < target <= nums[high]:
+                    return bsearch(nums, target, mid+1, high)
+                else:
+                    return bsearch(nums, target, low, mid-1)
+            else: # nums[low] == nums[mid]
+                # Catch!!! Low and mid are identical, example: {2, 2, 2, 3, 4, 2}.
+                # So first check if rightmost element is different: if yes, just search right half;
+                if nums[mid] != nums[high]:
+                    return bsearch(nums, target, mid+1, high)
+                else:
+                    # Otherwise, it is the worst case, check both halves.
+                    leftFound = bsearch(nums, target, low, mid-1)
+                    if leftFound:
+                        return True
+                    else:
+                        return bsearch(nums, target, mid+1, high)
+        
+        
+        return bsearch(nums, target, 0, len(nums)-1)
+                
