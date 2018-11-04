b9c5342f074d962bc741c37ace3392d685d27f1d
patrick-luo <patrick.luo2009@gmail.com>
2018-11-03 20:11:09 -0400

    Very worth going into it! Early stop version of quick sort

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -5,27 +5,28 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def semiQsort(a, l, r, k):
-            i = l
-            j = r
-            m = a[(l+r)/2] # the pivot number
+        def semiQsort(nums, low, high, k):
+            i, j = low, high
+            pivot = nums[(low+high)/2]
             while True:
-                while a[i] > m:
+                while nums[i] > pivot:
                     i += 1
-                while a[j] < m:
+                while nums[j] < pivot:
                     j -= 1
                 if i <= j:
-                    a[i], a[j] = a[j], a[i]
+                    nums[i], nums[j] = nums[j], nums[i]
                     i += 1
                     j -= 1
                 if i > j:
                     break
-            # l<r ensures it's OK to sort
+            # l<=r ensures it's OK to sort
             # i<=k<=r means it's necessary to sort
-            if i < r and i <= k <= r:
-                semiQsort(a, i, r, k) # sort the right half
-            if l < j and l <= k <= j:
-                semiQsort(a, l, j, k) # sort the left half
-        
+            if low <= k <= j:
+                semiQsort(nums, low, j, k)
+            if i <= k <= high:
+                semiQsort(nums, i, high, k)
+                
         semiQsort(nums, 0, len(nums)-1, k-1)
         return nums[k-1]
+
+
