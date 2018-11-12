a7e434f536512be893700426ec653b2c780e42a0
patrick-luo <patrick.luo2009@gmail.com>
2018-11-12 18:17:55 -0500

    Equivalently, check the longest prefix that is palindrome

---
+++ 214. Shortest Palindrome.py
@@ -0,0 +1,29 @@
+"""A valid O(N^2) solution, but not sure
+why time limit exceeded."""
+
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def isPalin(s):
+            i = 0
+            j = len(s)-1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            return True
+        
+        if len(s) == 0:
+            return s
+        
+        for i in xrange(len(s), 0, -1):
+            if isPalin(s[:i]):
+                more = list()
+                for j in xrange(len(s)-1, i-1, -1):
+                    more.append(s[j])
+                return ''.join(more) + s
