2ae254e7da3457d8c3a2ba97b28bd9ab58e00fd7
patrick-luo <patrick.luo2009@gmail.com>
2018-11-19 17:26:41 -0500

    Undirected DFS: remember to use 'used'

--- 079. Word Search.py
+++ 079. Word Search.py
@@ -5,31 +5,26 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        if len(word) == 0:
-            return True
         if len(board) == 0 or len(board[0]) == 0:
             return False
         
         def found(board, i, j, word, used):
             if len(word) == 0:
                 return True
-            for a, b in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)]:
-                if 0<=a<len(board) and 0<=b<len(board[0]) \
-                        and not used[a][b] and board[a][b]==word[0]:
-                    used[a][b] = True
+            if 0<=i<len(board) and 0<=j<len(board[0]) \
+                    and not used[i][j] and board[i][j] == word[0]:
+                used[i][j] = True
+                for a, b in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)]:
                     if found(board, a, b, word[1:], used):
                         return True
-                    used[a][b] = False
+                used[i][j] = False
             return False
                     
         
         used = [[False]*len(board[0]) for _ in xrange(len(board))]
         for i, row in enumerate(board):
             for j, e in enumerate(row):
-                if e == word[0]:
-                    used[i][j] = True
-                    if found(board, i, j, word[1:], used):
-                        return True
-                    used[i][j] = False
+                if found(board, i, j, word, used):
+                    return True
         return False
                     
